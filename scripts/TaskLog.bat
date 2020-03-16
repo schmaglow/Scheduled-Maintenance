8f826d22eb2a581fc9df26f4231f3630b7ae4cdc
schmaglow <61213490+schmaglow@users.noreply.github.com>
2020-03-16 16:01:07 -0500

    Update TaskLog.bat
    force admin

--- scripts/TaskLog.bat
+++ scripts/TaskLog.bat
@@ -1,6 +1,15 @@
 @echo off
 
+net session >nul
+if %errorlevel% neq 0 goto elevate nul
+goto :start
 
+:elevate
+cd/d %~dp0
+mshta "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1);close();" >nul
+exit
+
+:start
 cd C:\oecIT\Maintenance
 
 
