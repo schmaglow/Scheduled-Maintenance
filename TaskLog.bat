4e307a5a80fb5ee74dd9083475a4667df3843c4c
schmaglow <61213490+schmaglow@users.noreply.github.com>
2020-03-16 14:03:06 -0500

    Revert "Orginization"
    This reverts commit 3b7db05323db366ae8c4fc05148a95160151d717.

---
+++ TaskLog.bat
@@ -0,0 +1,57 @@
+@echo off
+
+
+cd C:\oecIT\Maintenance
+
+
+echo.
+echo.
+echo.
+echo *****************************************************************
+echo ********    S C H E D U L E D   M A I N T E N A N C E    ********
+echo *****************************************************************
+echo *****************************************************************
+echo **********    Please save all work and close out of    **********
+echo *********************    open programs.   ***********************
+echo *****************************************************************
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+
+
+::timeout /t 30
+PING -n 21 127.0.0.1>nul
+
+
+ :: log and call RecycleClean
+
+(
+echo.
+echo --------- 
+echo %date% 
+echo %time% 
+call RecycleClean.bat
+) >> C:\oecIT\Logs\RecycleClearLog.txt
+
+ :: log and call TempClear
+
+(
+echo.
+echo --------- 
+echo %date% 
+echo %time% 
+call TempClear.bat 
+) >> C:\oecIT\Logs\TempClearLog.txt
+
+ :: log and call NetRes
+
+(
+echo.
+echo ---------
+echo %date%
+echo %time%
+call NetRes
+) >> C:\oecIT\Logs\NetRes.txt
---
+++ TempClear.bat
@@ -0,0 +1,10 @@
+@echo off
+
+
+ :: remove files from path that are at least 7 days old
+
+FORFILES /p %temp% /m *.* /s /d -7 /c "cmd /c del @path /a /s /f"
+
+ :: remove files from path that are at least 7 days old
+
+FORFILES /p %windir%/temp /m *.* /s /d -7 /c "cmd /c del @path /a /s /q /f"
