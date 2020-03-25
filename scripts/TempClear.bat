833d6eb4c0e617b05a68c816b868c72f28d4632d
schmaglow <61213490+schmaglow@users.noreply.github.com>
2020-03-25 08:01:10 -0500

    Update TempClear.bat

--- scripts/TempClear.bat
+++ scripts/TempClear.bat
@@ -10,3 +10,5 @@ FORFILES /p %temp% /m *.* /s /d -7 /c "cmd /c del @path /a /s /q /f"
 REM remove files from path that are at least 7 days old
 
 FORFILES /p %windir%/temp /m *.* /s /d -7 /c "cmd /c del @path /a /s /q /f"
+
+exit /b %1
